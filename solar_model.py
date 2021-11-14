f24ead85826d6e245ef4b4e0a0c642495938d743
Nikitushechka <khomiakov.na@phystech.edu>
2021-11-14 19:39:18 +0300

    Сделал функции calculate_force и move_space_object

--- solar_model.py
+++ solar_model.py
@@ -21,6 +21,11 @@ def calculate_force(body, space_objects):
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
         r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        F = gravitational_constant * (body.m * obj.m)/(r**2)
+        Fx = -F * ((body.x - obj.x)/r)
+        Fy = -F * ((body.y-obj.y)/r)
+        body.Fx += Fx
+        body.Fy += Fy
         pass  # FIXME: Взаимодействие объектов
 
 def move_space_object(body, dt):
@@ -32,10 +37,14 @@ def move_space_object(body, dt):
     """
     old = body.x  # FIXME: Вывести формулы для ускорения, скоростей и координат
     ax = body.Fx/body.m
-    body.x += 24
-    ay = body.Fy*body.m
-    body.y = 42
-    body.Vy += 4*dt
+    ay = body.Fy * body.m
+    body.Vx += ax*dt
+    body.Vy += ay*dt
+
+    body.x += body.Vx * dt
+    body.y += body.Vy * dt
+
+
 
 
 def recalculate_space_objects_positions(space_objects, dt):
